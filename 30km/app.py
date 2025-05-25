ee42e78f7655b7b238a4419aa9cb535f096f5428
esterjez <ester.jezberova@gmail.com>
2025-05-25 19:20:02 +0200

    web hosting

--- 30km/app.py
+++ 30km/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -22,5 +23,6 @@ def tool():
 def result():
     return render_template('result1.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host="0.0.0.0", port=port)
